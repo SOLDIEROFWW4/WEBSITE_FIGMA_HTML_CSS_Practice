e440a7d2cff04a51a4dd09b3aca4ef0f2af192c8
SOLDIEROFWW4 <61186683+SOLDIEROFWW4@users.noreply.github.com>
2023-09-06 17:08:28 +0300

    feat: add id for menu section, add translate.js file

---
+++ translate.js
@@ -0,0 +1,37 @@
+let langArr = {
+    "menu":
+    {
+
+
+        "home":
+        {
+            "RU": "Главная",
+            "EN": "Home"
+        },
+
+        "features":
+        {
+            "RU": "Особенности",
+            "EN": "Features"
+        },
+
+        "gallary":
+        {
+            "RU": "Галлерея",
+            "EN": "Gallary"
+        },
+
+        "testimonials":
+        {
+            "RU": "Рекомендации",
+            "EN": "Testimonials"
+        },
+
+        "book":
+    {
+        "RU": "Забронировать",
+        "EN": "Book now"
+    }
+    }
+
+}
